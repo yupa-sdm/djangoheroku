6e5cf54428724c6c101478e0676d0fda28c4570e
yupa-sdm <86347328+yupa-sdm@users.noreply.github.com>
2021-07-15 19:58:07 +0700

    prepare deploy to heroku

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
-from Blog.views import index
+from Blog.views import about, contact, persons, index
 
 urlpatterns = [
-    path('', index)
+    path ('', index) ,
+    path ('about/', about),
+    path ('contact/', contact),
+    path ('persons/', persons),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-        return HttpResponse("Hello, django Framework")
+    return HttpResponse('Hello, Index Page')
+
+def about(request):
+    return HttpResponse('About Page')
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
+def persons(request):
+    return HttpResponse('Persons Page')
